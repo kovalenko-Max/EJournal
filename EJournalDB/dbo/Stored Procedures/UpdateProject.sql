6332cd60aca7dbd7d34637f9fe86910d2de107d5
Ola <oleksandrakondratenko@gmail.com>
2021-06-03 17:08:45 +0300

    Test

--- EJournalDB/dbo/Stored Procedures/UpdateProject.sql
+++ EJournalDB/dbo/Stored Procedures/UpdateProject.sql
@@ -1,11 +1,11 @@
 ﻿CREATE PROCEDURE [dbo].[UpdateProject] @Id INT
 	,@Name NVARCHAR(50)
 	,@Description NVARCHAR(255)
-	,@ChekUpdate BIT OUTPUT
+	--,@ChekUpdate BIT OUTPUT
 AS
 UPDATE [dbo].[Projectes]
 SET Name = @Name
 	,Description = @Description
 WHERE Id = @Id
 
-SELECT @ChekUpdate = @@ROWCOUNT
+--SELECT @ChekUpdate = @@ROWCOUNT
