28aae1bc60000f39c8385dd1ac364a1ee01880e6
Maks Kovalenko <max.don@live.ru>
2021-06-05 19:05:45 +0300

    Add attendanses v1

---
+++ EJournalDB/dbo/Stored Procedures/AddStudentsAttendance.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[AddStudentsAttendance]
+	@Topic nvarchar(250),
+	@DateLesson datetime, 
+	@IdGroup int
+	
+AS
+	declare @IdLesson int
+
+	insert into Lessons (Topic, DateLesson, IdGroup)
+	values(@Topic, @DateLesson, @IdGroup)
+	SET @IdLesson = SCOPE_IDENTITY()
+
