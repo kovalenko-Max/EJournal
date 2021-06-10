8e488575cadec462df902dfcd30236787dc43c60
Brovko Alexandra <brovko.sasha2002@gmail.com>
2021-06-10 18:26:51 +0300

    Create Exercise Service and Repository

---
+++ EJournalBLL/Services/ExercisesService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using EJournalBLL.Models;
+using EJournalDAL.Repository;
+using EJournalDAL.Models.BaseModels;
+using System.Data;
+
+namespace EJournalBLL.Services
+{
+    public class ExercisesService
+    {
+        
+    }
+}
